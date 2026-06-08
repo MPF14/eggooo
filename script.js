04a3f82a9bdd901985b00109c86000cd9c2a525c
Maddie <Mpfincognito@gmail.com>
2026-06-07 19:26:02 -0700

    finish start screen

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let rectSprite;
 await Canvas(400, 400)
 let font1 = loadFont('assets/pixelFont.ttf');
 let font2 = loadFont('assets/kiwiSoda.ttf')
+await font1;
+await font2;
 let bar1 = loadImage('assets/bar1.png');
 let bar2 = loadImage('assets/bar2.png');
 let bar3 = loadImage('assets/bar3.png');
@@ -81,52 +83,48 @@ let audio10 = loadSound('assets/audio/BLUE.mp3');
 let startButton = new Sprite(0, 70, 350, 100);
 startButton.img = playButton;
 startButton.scale = .6;
-startButton.physics = 'static';
 startButton.img.offset.y = 10;
 startButton.layer = 1;
 
-// let startText = new Sprite(0, 0, 200, 200);
-// startText.text = 'Ehi';
-// startText.scale = 1;
-// startText.textSize = 20;
-// startText.color = '#8dbd3a';
-// startText.physics = 'static';
-// startText.stroke = '#98cfa2';
-// startText.textColor = '#206992';
+let titleText = new Sprite(0, -80, 260, 100);
+let g = createGraphics(280, 200);
+g.textSize(80);
+g.fill('#658ca1');
+g.stroke('#658ca1');
+g.strokeWeight(2);
+g.textAlign(CENTER, CENTER);
+g.textFont(font2);
+g.text("Eggooo!", 140, 100);
+titleText.img = g;
 
-let target = new Sprite(0, 0, 400, 225);
+
+let target = new Sprite(0, -300, 400, 225);
 target.img = egg1;
-target.scale = .1;
+target.scale = .3;
 target.img.offset.y = -80;
-target.physics = 'static';
 target.layer = 2;
 
 // settings menu
 let settingsMenu = new Sprite(200, 600, 200, 400);
 settingsMenu.image = settingsMenuL;
 settingsMenu.scale = .3;
-settingsMenu.physics = 'static';
 
 // music
 let music = new Sprite(115, 550, 800, 800);
 music.image = toggleOn1;
 music.scale = 0.1;
-music.physics = 'static';
 
 let leftArrow = new Sprite(105, 590, 800, 800);
 leftArrow.image = leftArrow1;
 leftArrow.scale = 0.04;
-leftArrow.physics = 'static';
 
 let rightArrow = new Sprite(135, 590, 800, 800);
 rightArrow.image = rightArrow1;
 rightArrow.scale = 0.04;
-rightArrow.physics = 'static';
 
 let musicText = new Sprite(225, 550, 10, 10);
 musicText.text = 'Music on';
 musicText.textSize = 20;
-musicText.physics = 'static';
 musicText.color = '#dcb98a';
 musicText.stroke = '#dcb98a';
 musicText.textColor = '#90625d';
@@ -134,7 +132,6 @@ musicText.textColor = '#90625d';
 let songText = new Sprite(225, 590, 10, 10);
 songText.text = 'Song selector';
 songText.textSize = 20;
-songText.physics = 'static';
 songText.color = '#dcb98a';
 songText.stroke = '#dcb98a';
 songText.textColor = '#90625d';
@@ -144,17 +141,14 @@ songText.textColor = '#90625d';
 let volumeBar = new Sprite(200, 630, 200, 200); //(200,280,140,13)
 volumeBar.image = bar5;
 volumeBar.scale = .2;
-volumeBar.physics = 'static';
 
 let volumeDot = new Sprite(200, 628, 1400, 70);
 volumeDot.image = slideDot;
 volumeDot.scale = .2;
-volumeDot.physics = 'static';
 
 let volumeText = new Sprite(200, 655, 10, 10);
 volumeText.text = 'Volume: ' + round(volume / 14);
 volumeText.textSize = 20;
-volumeText.physics = 'static';
 volumeText.color = '#dcb98a';
 volumeText.stroke = '#dcb98a';
 volumeText.textColor = '#90625d';
@@ -163,12 +157,10 @@ volumeText.textColor = '#90625d';
 let infoButton = new Sprite(-150, 150, 400, 400);
 infoButton.image = exButton;
 infoButton.scale = 0.1;
-infoButton.physics = 'static';
 
 let infoText = new Sprite(225, 685, 10, 10);
 infoText.text = 'info';
 infoText.textSize = 20;
-infoText.physics = 'static';
 infoText.color = '#dcb98a';
 infoText.stroke = '#dcb98a';
 infoText.textColor = '#90625d';
@@ -177,7 +169,6 @@ infoText.textColor = '#90625d';
 let infoMenu = new Sprite(200, 600, 200, 400);
 infoMenu.image = infoMenuL;
 infoMenu.scale = .3;
-infoMenu.physics = 'static';
 
 let infoMenuText = new Sprite(200, 600, 10, 10);
 infoMenuText.text = 'Click the next to\n earn points. Once you earn\nenough coins,you can buy \npower ups in the shop.\nOnce you earn enough, you\ncan prestige and reset to\nearn a bonus and your nest\nwill evolve! CPC stands for\ncoins per click and CPS stands\nfor coins per second.\n\nGraphics by Cup Nooble\nMusic by Billie Eilish\nMade by Matilda Fletcher';
@@ -185,13 +176,11 @@ infoMenuText.textSize = 14;
 infoMenuText.textColor = '#90625d';
 infoMenuText.color = '#dcb98a';
 infoMenuText.stroke = '#dcb98a';
-infoMenuText.physics = 'static';
 
 // score menu
 let scoreMenu = new Sprite(-295, 60, 400, 400);
 scoreMenu.image = scoreMenuL;
 scoreMenu.scale = 0.2;
-scoreMenu.physics = 'static';
 scoreMenu.textSize = 15;
 scoreMenu.textColor = '#90625d';
 scoreMenu.text = scoreText + '\n\n' + multiplierText + '\n\n' + passiveText;
@@ -200,52 +189,43 @@ scoreMenu.text = scoreText + '\n\n' + multiplierText + '\n\n' + passiveText;
 let coinIcon = new Sprite(-375, 23, 50, 50);
 coinIcon.image = coinIcon1;
 coinIcon.scale = 0.03;
-coinIcon.physics = 'static';
 
 // tool icon
 let toolIcon = new Sprite(-373, 65, 50, 50);
 toolIcon.image = egg3;
 toolIcon.scale = 0.03;
-toolIcon.physics = 'static';
 
 // water icon
 let waterIcon = new Sprite(-373, 100, 50, 50);
 waterIcon.image = egg3;
 waterIcon.scale = 0.03;
-waterIcon.physics = 'static';
 
 // shop button
 let shopButton = new Sprite(765, 370, 800, 800);
 shopButton.image = shopButtonL;
 shopButton.scale = 0.05;
-shopButton.physics = 'static';
 
 // settings button
 let settingsButton = new Sprite(35, 370, 800, 800);
 settingsButton.image = settingsButtonL;
 settingsButton.scale = 0.05;
-settingsButton.physics = 'static';
 
 // shop menu
 let shopMenu = new Sprite(500, 200, 200, 400);
 shopMenu.image = shopMenuL;
 shopMenu.scale = .44;
-shopMenu.physics = 'static';
 
 // prestige button
 let prestigeButton = new Sprite(500, 355, 700, 200);
 prestigeButton.image = prestigeButtonL;
 prestigeButton.scale = .2;
-prestigeButton.physics = 'static';
 
 // shop menu buttons
 let shopMenuButton1 = new Sprite(450, 50, 700, 700);
 shopMenuButton1.image = squareButton;
 shopMenuButton1.scale = .05;
-shopMenuButton1.physics = 'static';
 
 let shopMenuText1 = new Sprite(530, 50, 100, 35);
-shopMenuText1.physics = 'static';
 shopMenuText1.text = 'COST: 50 \n1 CPC';
 shopMenuText1.textColor = '#90625d';
 shopMenuText1.color = '#dcb98a';
@@ -255,10 +235,8 @@ shopMenuText1.stroke = '#dcb98a';
 let shopMenuButton2 = new Sprite(450, 100, 700, 700);
 shopMenuButton2.image = squareButton;
 shopMenuButton2.scale = .05;
-shopMenuButton2.physics = 'static';
 
 let shopMenuText2 = new Sprite(530, 100, 100, 35);
-shopMenuText2.physics = 'static';
 shopMenuText2.text = 'COST: 1000 \n5 CPS';
 shopMenuText2.textColor = '#90625d';
 shopMenuText2.color = '#dcb98a';
@@ -268,10 +246,8 @@ shopMenuText2.stroke = '#dcb98a';
 let shopMenuButton3 = new Sprite(450, 150, 700, 700);
 shopMenuButton3.image = squareButton;
 shopMenuButton3.scale = .05;
-shopMenuButton3.physics = 'static';
 
 let shopMenuText3 = new Sprite(530, 150, 100, 35);
-shopMenuText3.physics = 'static';
 shopMenuText3.text = 'COST: 25000 \n300 CPC';
 shopMenuText3.textColor = '#90625d';
 shopMenuText3.color = '#dcb98a';
@@ -280,10 +256,8 @@ shopMenuText3.stroke = '#dcb98a';
 let shopMenuButton4 = new Sprite(450, 200, 700, 700);
 shopMenuButton4.image = squareButton;
 shopMenuButton4.scale = .05;
-shopMenuButton4.physics = 'static';
 
 let shopMenuText4 = new Sprite(530, 200, 100, 35);
-shopMenuText4.physics = 'static';
 shopMenuText4.text = 'COST: 5a \n30,000 CPS';
 shopMenuText4.textColor = '#90625d';
 shopMenuText4.color = '#dcb98a';
@@ -292,10 +266,8 @@ shopMenuText4.stroke = '#dcb98a';
 let shopMenuButton5 = new Sprite(450, 250, 700, 700);
 shopMenuButton5.image = squareButton;
 shopMenuButton5.scale = .05;
-shopMenuButton5.physics = 'static';
 
 let shopMenuText5 = new Sprite(530, 250, 100, 35);
-shopMenuText5.physics = 'static';
 shopMenuText5.text = 'COST: 250a \n3a CPC';
 shopMenuText5.textColor = '#90625d';
 shopMenuText5.color = '#dcb98a';
@@ -304,18 +276,18 @@ shopMenuText5.stroke = '#dcb98a';
 let shopMenuButton6 = new Sprite(450, 300, 700, 700);
 shopMenuButton6.image = squareButton;
 shopMenuButton6.scale = .05;
-shopMenuButton6.physics = 'static';
 
 let shopMenuText6 = new Sprite(530, 300, 100, 35);
-shopMenuText6.physics = 'static';
 shopMenuText6.text = 'COST: 100b \n500a CPS';
 shopMenuText6.textColor = '#90625d';
 shopMenuText6.color = '#dcb98a';
 shopMenuText6.stroke = '#dcb98a';
-
+allSprites.passes(allSprites);
 //allSprites.debug = true;
+
+
 q5.update = function () {
-	background("#89D5D2");
+ 	background("#89D5D2");
 	// text('click to increase score!', 0, -50);
 	// text('Score: ' + score, 0, -30);
 
@@ -326,53 +298,47 @@ q5.update = function () {
     	textFont(font1);
   	}
 	
-if (page == 0) {
-    textFont(font2);
-    settingsButton.img = exButton;
-	textSize(80);
-	fill ('#658ca1');
-	stroke ('#658ca1');
-	textAlign(CENTER, CENTER);
-	text('Eggooo!', 0, -80);
-  } else if (page == 2) {
-    //halfOn();
-    startButton.moveTo(200, 500, 10);
-    //startText.visible = false;
-    infoButton.img = settingsButtonL;
-  }
-  //star button clicked
-  	if (mouse.pressing() && pointer.overlapping(startButton)) {
-		startButton.y = 80;
-    	startButton.img = playButtonClicked;
-    	setTimeout(() => { startButton.img = playButton; startButton.y = 70;}, 100);
-    	page = 2;
-//   }
-// //page 0 - start
-// 	if (page == 0) {
-// 		textSize(80);
-// 		fill ('#658ca1');
-// 		stroke ('#658ca1');
-// 		textAlign(CENTER, CENTER);
-// 		text('Eggooo!', 0, -80);
-// 		startButton.visible = true;
-// 		target.visible = false;
-// 		settingsMenu.visible = false;
-// 		if (mouse.presses() && pointer.overlapping(startButton)) {
-
-// 			page = 1;
-// 		}
-		
-
-	}
-//page 1 - main
-	if (page == 1) {
-		startButton.visible = false;
-		target.visible = true;
-		settingsMenu.visible = false;
-		if (mouse.presses() && pointer.overlapping(target)) {
-		score += 1;
+	if (page == 0) {
+		target.visible = false;
+		// textFont(font2);
+		// settingsButton.img = exButton;
+		// textSize(80);
+		// fill ('#658ca1');
+		// stroke ('#658ca1');
+		// textAlign(CENTER, CENTER);
+		// text('Eggooo!', 0, -80);
+		if 	(mouse.pressing() && pointer.overlapping(startButton)) {
+			startButton.y = 80;
+			startButton.img = playButtonClicked;
+			setTimeout(() => { startButton.img = playButton;}, 100);
+			page = 1;
 		}
-	}
+		if (pointer.overlapping(startButton)) {
+			startButton.img = playButtonClicked;
+			startButton.y = 80;
+		} else {
+			startButton.img = playButton;
+			startButton.y = 70;
+		}
+  } else if (page == 1) {
+		target.visible = true;
+		infoButton.img = settingsButtonL;
+		startButton.moveTo(0,300,3);
+		target.moveTo(0,0,3);
+  }
+  //start button clicked
+	// if (page == 1) {
+	// 	startButton.visible = false;
+	// 	target.visible = true;
+	// 	settingsMenu.visible = false;
+	// 	if (mouse.presses() && pointer.overlapping(target)) {
+	// 	score += 1;
+	// 	}
+	// }
+
+
+// //page 0 - start
+//page 1 - main
 //page 2 - audio
 //page 3 - info
 //page 4 - shop
